68ef0f8995cf92969acd995497d040e78f68d0d8
truonghailinh <camcum2002@gmail.com>
2017-09-26 22:22:53 +0700

    linhth:baitap

---
+++ chaoban.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	char A[30];
+	cout<<"nhap ten cua ban: ";
+	cin>>A;
+	cout<<" Chao Ban " << A;
+}
---
+++ tbp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int a;
+	int b;
+	cout<<"nhap a: ";
+	cin>>a;
+	cout<<"nhap b: ";
+	cin>>b;
+	cout<<"tong binh phuong cua hai so tren la: "<< (a*a)+(b*b);
+}
