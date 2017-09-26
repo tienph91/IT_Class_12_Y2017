864657fe3e32f4dd93b1ac6c7929e10a502823b1
truonghailinh <camcum2002@gmail.com>
2017-09-26 21:52:53 +0700

    linhth:comment

--- nhietdo.cpp
+++ nhietdo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 int main(void)
 {
@@ -6,6 +7,6 @@ int main(void)
 	float C;
 	cout<<"nhap do F: ";
 	cin>>F;
-	cout<< F << " do F=" << 8/9*(F-32) << " do C ";
+	cout<< F << " do F=" << 5*(F-32)/9 << " do C ";
 	return 0;
 }
