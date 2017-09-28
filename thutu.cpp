a569a7effc1558e80a215e52c478b8d2be1c1d96
truonghailinh <camcum2002@gmail.com>
2017-09-28 19:20:57 +0700

    linhth:thutu

---
+++ thutu.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+  int A[100], n;
+  int tg;
+  cout<<"nhap so luong phan tu mang n= ";cin>>n;
+  for(int i= 1 ; i<=n ; i++ )
+  {
+  	cout<<"nhap phan tu A["<<i<<"]= ";
+  	cin>>A[i];
+  }
+    cout<<"mang vua nhap la: \n";
+     for(int i=1;i<=n;i++)
+	cout<<A[i]<<" ";
+	 
+	 for(int i=1;i<=n;i++)
+	for(int j=i+1;j<=n;j++)
+	
+	if(A[i]<A[j])
+	{
+		tg=A[i];
+		A[i]=A[j];
+		A[j]=tg;
+	}
+    cout<<"day vua sap xep la: \n";
+     for(int i=1;i<=n;i++)
+    cout<< A[i] <<" ";
+}
+
